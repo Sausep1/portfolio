955e415c5d62e6d95de29e684bb6f8eed249ba8d
Sausep1 <103946921+Sausep1@users.noreply.github.com>
2022-04-18 17:47:31 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+$('form').submit(function(e) {
+    e.preventDefault();
+    
+    if (!$(this).valid()) {
+        return;
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+
+        $('form').trigger('reset');
+    });
+    return false;
+});
